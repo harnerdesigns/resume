7539feae4a66b4e3e0b187ae23f36eaf4b06c196
Jack Harner <social@shoolu.com>
2018-12-12 16:30:36 -0700

    lots of fixes

--- js/app.js
+++ js/app.js
@@ -1,16 +1,1 @@
-
-$(".highlight").click(function() {
-	if ($(this).hasClass("open")) {
-		$(this)
-			.children(".highlight__bullets")
-			.slideUp();
-		$(this).removeClass("open");
-		 $(this).find('svg').addClass('fa-caret-down').removeClass('fa-caret-up');
-	} else {
-		$(this).addClass("open");
-					 $(this).find('svg').addClass('fa-caret-up').removeClass('fa-caret-down');
-		$(this)
-			.children(".highlight__bullets")
-			.slideDown();
-	}
-});
+console.log("Hi, I'm Jack and Welcome to my Resume's Console.");
